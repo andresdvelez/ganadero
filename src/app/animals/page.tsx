35dbe029d585c12f18f05d8ec7aa2968386009ca
andresdvelez <andres082403@gmail.com>
2025-08-09 08:11:15 -0500

    fix release

--- src/app/animals/page.tsx
+++ src/app/animals/page.tsx
@@ -1,5 +1,12 @@
+import { Suspense } from "react";
 import AnimalsClient from "@/app/_/_client";
 
+export const dynamic = "force-dynamic";
+
 export default function Page() {
-  return <AnimalsClient />;
+  return (
+    <Suspense fallback={<div />}>
+      <AnimalsClient />
+    </Suspense>
+  );
 }
